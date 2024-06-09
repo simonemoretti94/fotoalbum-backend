6ab577f950c13ab7fd4f36af56e06f13f0475af1
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-10 00:25:27 +0200

    3.2th added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/img/fotoalbum-icon.svg') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">

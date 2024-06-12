59b99cd7d63d10b719c849425170495805e6d261
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-12 09:50:07 +0200

    6.1th reviewed show view and cleaned moved each js into each own file

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,48 +7,35 @@
         padding: .3rem;
         border-radius: 5px;
     }
+
     h6#offcanvas-trigger:hover {
         color: white;
         background-color: #EBEBEB;
-    } 
+    }
 </style>
-    <button
-        class="btn"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#Id2"
-        aria-controls="Id2"
-    >
-        <h6 id="offcanvas-trigger">Account actions</h6>
-    </button>
+<button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#Id2" aria-controls="Id2">
+    <h6 id="offcanvas-trigger">Account actions</h6>
+</button>
 
-    <div
-        class="offcanvas offcanvas-start"
-        data-bs-backdrop="static"
-        tabindex="-1"
-        id="Id2"
-        aria-labelledby="staticBackdropLabel"
-    >
-        <div class="offcanvas-header">
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-            ></button>
-        </div>
-        <div class="offcanvas-body">
+<div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="Id2"
+    aria-labelledby="staticBackdropLabel">
+    <div class="offcanvas-header">
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
         <div id="actions">
 
             <h6 class="my-4">Account actions</h6>
 
             <p><i id="sidebar-icon" class="fa-solid fa-gear"></i><a href="{{route('profile.edit')}}">Settings</a></p>
-            <p><i id="sidebar-icon" class="fa-solid fa-chart-line"></i><a href="{{route('admin.dashboard')}}">Dashboard</a></p>
+            <p><i id="sidebar-icon" class="fa-solid fa-chart-line"></i><a
+                    href="{{route('admin.dashboard')}}">Dashboard</a></p>
 
             <h6 class="my-4">Photos actions</h6>
 
             <p><i id="sidebar-icon" class="fa-solid fa-compass-drafting"></i><a href="drafts">Drafts</a></p>
-            <p><i id="sidebar-icon" class="fa-solid fa-layer-group"></i><a href="{{route('admin.categories.index')}}">Categories</a></p>
+            <p><i id="sidebar-icon" class="fa-solid fa-layer-group"></i><a
+                    href="{{route('admin.categories.index')}}">Categories</a></p>
 
             <p id="p-show-head"><i id="sidebar-icon" class="fas fa-eye fa-xs fa-fw"></i>Show</p>
             <div id="div-js-show-head" class="d-none">
@@ -63,141 +50,208 @@
                 <p>{{$photo->id}}: <a href="{{route('admin.photos.edit' , $photo)}}">{{$photo->title}}</a></p>
                 @endforeach
             </div>
-
-            {{-- <div id="actions">
-                <h6 class="text-danger">Danger Area</h6>
-                <p id="p-delete-head"><i id="sidebar-icon" class="fa-regular fa-trash-can"></i>Delete</p>
-                <div id="div-js-delete-head" class="d-none">
-                    @foreach ($photos as $key=>$photo)
-                    <x-modal :photo="$photo"></x-modal>
-                    @endforeach
-                </div>
-            </div> --}}
-        </div>
         </div>
     </div>
+</div>
 
 
 @endsection
 
 @section('content-left')
-    <div id="actions">
+<div id="actions">
     <h6>Account actions</h6>
     <p><i id="sidebar-icon" class="fa-solid fa-gear"></i><a href="{{route('profile.edit')}}">Settings</a></p>
     <p><i id="sidebar-icon" class="fa-solid fa-chart-line"></i><a href="{{route('admin.dashboard')}}">Dashboard</a></p>
-    </div>
+</div>
 
-    <div id="actions">
-        <h6>Photos actions</h6>
-        <p><i id="sidebar-icon"  class="fa-solid fa-square-plus"></i><a href="{{route('admin.photos.create')}}">Create</a></p>
-        <p id="p-show"><i id="sidebar-icon" class="fas fa-eye fa-xs fa-fw"></i>Show</p>
-        <div id="div-js-show" class="d-none">
-            @foreach ($photos as $key=>$photo)
-            <p>{{$photo->id}}: <a href="{{route('admin.photos.show' , $photo)}}">{{$photo->title}}</a></p>
-            @endforeach
-        </div>
-        <p id="p-edit"><i id="sidebar-icon" class="fa-solid fa-pen-to-square"></i>Edit</p>
-        <div id="div-js-edit" class="d-none">
-            @foreach ($photos as $key=>$photo)
-            <p>{{$photo->id}}: <a href="{{route('admin.photos.edit' , $photo)}}">{{$photo->title}}</a></p>
-            @endforeach
-        </div>
-        <p><i id="sidebar-icon" class="fa-solid fa-compass-drafting"></i><a href="drafts">Drafts</a></p>
-        <p><i id="sidebar-icon" class="fa-solid fa-layer-group"></i><a href="{{route('admin.categories.index')}}">Categories</a></p>
+<div id="actions">
+    <h6>Photos actions</h6>
+    <p><i id="sidebar-icon" class="fa-solid fa-square-plus"></i><a href="{{route('admin.photos.create')}}">Create</a>
+    </p>
+    <p id="p-show"><i id="sidebar-icon" class="fas fa-eye fa-xs fa-fw"></i>Show</p>
+    <div id="div-js-show" class="d-none">
+        @foreach ($photos as $key=>$photo)
+        <p>{{$photo->id}}: <a href="{{route('admin.photos.show' , $photo)}}">{{$photo->title}}</a></p>
+        @endforeach
     </div>
-
-    <div id="actions">
-        <h6 class="text-danger">Danger Area</h6>
-        <p id="p-delete"><i id="sidebar-icon" class="fa-regular fa-trash-can"></i>Delete</p>
-        <div id="div-js-delete" class="d-none">
-            @foreach ($photos as $key=>$photo)
-            {{-- <p>{{$photo->id}}: <a href="{{route('admin.photos.destroy' , $photo)}}">{{$photo->title}}</a></p> --}}
-            <x-modal :photo="$photo"></x-modal>
-            @endforeach
-        </div>
+    <p id="p-edit"><i id="sidebar-icon" class="fa-solid fa-pen-to-square"></i>Edit</p>
+    <div id="div-js-edit" class="d-none">
+        @foreach ($photos as $key=>$photo)
+        <p>{{$photo->id}}: <a href="{{route('admin.photos.edit' , $photo)}}">{{$photo->title}}</a></p>
+        @endforeach
     </div>
+    <p><i id="sidebar-icon" class="fa-solid fa-compass-drafting"></i><a href="drafts">Drafts</a></p>
+    <p><i id="sidebar-icon" class="fa-solid fa-layer-group"></i><a
+            href="{{route('admin.categories.index')}}">Categories</a></p>
+</div>
 
+<div id="actions">
+    <h6 class="text-danger">Danger Area</h6>
+    <p id="p-delete"><i id="sidebar-icon" class="fa-regular fa-trash-can"></i>Delete</p>
+    <div id="div-js-delete" class="d-none">
+        @foreach ($photos as $key=>$photo)
+        <x-modal :photo="$photo"></x-modal>
+        @endforeach
+    </div>
+</div>
+
+
+<script>
+    /* show head section */
+    let showHead = true;
+
+    document.getElementById('p-show-head').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (showHead) {
+            document.getElementById('div-js-show-head').classList.remove('d-none');
+            showHead = !showHead;
+        }
+        else {
+            document.getElementById('div-js-show-head').classList.add('d-none');
+            showHead = !showHead;
+        }
+    });
+
+    /* show section */
+    let show = true;
+
+    document.getElementById('p-show').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (show) {
+            document.getElementById('div-js-show').classList.remove('d-none');
+            show = !show;
+        }
+        else {
+            document.getElementById('div-js-show').classList.add('d-none');
+            show = !show;
+        }
+    });
+
+    /* edit head section */
+    let editHead = true;
+
+    document.getElementById('p-edit-head').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (editHead) {
+            document.getElementById('div-js-edit-head').classList.remove('d-none');
+            editHead = !editHead;
+        }
+        else {
+            document.getElementById('div-js-edit-head').classList.add('d-none');
+            editHead = !editHead;
+        }
+    });
+
+    /* edit section */
+    let edit = true;
+
+    document.getElementById('p-edit').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (edit) {
+            document.getElementById('div-js-edit').classList.remove('d-none');
+            edit = !edit;
+        }
+        else {
+            document.getElementById('div-js-edit').classList.add('d-none');
+            edit = !edit;
+        }
+    });
+
+
+    /* delete section */
+    let deletePhoto = true;
+
+    document.getElementById('p-delete').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (deletePhoto) {
+            document.getElementById('div-js-delete').classList.remove('d-none');
+            deletePhoto = !deletePhoto;
+        }
+        else {
+            document.getElementById('div-js-delete').classList.add('d-none');
+            deletePhoto = !deletePhoto;
+        }
+    });
+</script>
 @endsection
 
 @section('content-right')
-    <div class="container">
-        @if(session('status'))
-            <div class="alert alert-success">
-                {{session('status')}}
-            </div>
-        @endif
-        <div
-            class="table-responsive"
-        >
-            <table
-                class="table table-light"
-            >
-                <thead>
-                    <tr class="tr-data">
-                        <th scope="col">Id</th>
-                        <th scope="col" id="th-image">Cover image</th>
-                        <th scope="col" class="th-title">Title</th>
-                        <th scope="col" class="th-title">Infos</th>
-                        <th class="th-description" scope="col">Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($photos as $photo)
-                        
-                    <tr class="row-data">
-                        <td scope="row">
-                            <p class="text-center">{{$photo->id}}</p>
-                        </td>
-                        
-                        <td id="td-image">
+<div class="container">
+    @if(session('status'))
+    <div class="alert alert-success">
+        {{session('status')}}
+    </div>
+    @endif
+    <div class="table-responsive">
+        <table class="table table-light">
+            <thead>
+                <tr class="tr-data">
+                    <th scope="col">Id</th>
+                    <th scope="col" id="th-image">Cover image</th>
+                    <th scope="col" class="th-title">Title</th>
+                    <th scope="col" class="th-title">Infos</th>
+                    <th class="th-description" scope="col">Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($photos as $photo)
+
+                <tr class="row-data">
+                    <td scope="row">
+                        <p class="text-center">{{$photo->id}}</p>
+                    </td>
+
+                    <td id="td-image">
 
                         @if(Str::startsWith($photo->cover_image , 'https://'))
-                            <img width="150" height="200" src="{{$photo->cover_image}}" alt="{{$photo->title}}">
+                        <img width="150" height="200" src="{{$photo->cover_image}}" alt="{{$photo->title}}">
                         @else
-                            <img width="150" height="200" src="{{asset('storage/' . $photo->cover_image)}}" alt="{{$photo->title}}">
+                        <img width="150" height="200" src="{{asset('storage/' . $photo->cover_image)}}"
+                            alt="{{$photo->title}}">
                         @endif
 
-                        </td>
+                    </td>
 
-                        <td class="td-title" > 
-                            <p>{{$photo->title}}</p>
-                            <p><b>Slug:</b></p>
-                            <p>{{$photo->slug}}</p>
-                        </td>
-                        <td>
-                            <p><b>Created: </b><span>{{$photo->created_at}}</span></p>
-                            <p><b>Updated: </b><span>{{$photo->updated_at}}</span></p>
-                            <p><b>Size: </b><span>{{$photo->file_size}}kb</span> | <b>Format: </b><span>{{$photo->format}}</span></p>
-                        </td>
-                        <td class="td-description">{{$photo->description}}</td>
-                    </tr>
-                    @empty
-                    <tr class="">
-                        <td scope="row" colspan="4">No record to show.</td>
-                    </tr>
-                        
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-        
+                    <td class="td-title">
+                        <p>{{$photo->title}}</p>
+                        <p><b>Slug:</b></p>
+                        <p>{{$photo->slug}}</p>
+                    </td>
+                    <td>
+                        <p><b>Created: </b><span>{{$photo->created_at}}</span></p>
+                        <p><b>Updated: </b><span>{{$photo->updated_at}}</span></p>
+                        <p><b>Size: </b><span>{{$photo->file_size}}kb</span> | <b>Format:
+                            </b><span>{{$photo->format}}</span></p>
+                    </td>
+                    <td class="td-description">{{$photo->description}}</td>
+                </tr>
+                @empty
+                <tr class="">
+                    <td scope="row" colspan="4">No record to show.</td>
+                </tr>
+
+                @endforelse
+            </tbody>
+        </table>
     </div>
 
-    
+</div>
+
+
 <style>
     tr.row-data {
         @media screen and (max-width: 600px) {
             height: 150px;
-    
+
             & img {
                 width: 100px;
                 height: 150px;
             }
         }
-    
+
         @media screen and (max-width: 600px) {
             height: 100px;
-    
+
             & img {
                 width: 70px;
                 height: 100px;
@@ -205,28 +259,31 @@
         }
     }
 
-    th#th-image, td#td-image {
+    th#th-image,
+    td#td-image {
         @media screen and (max-width: 600px) {
-            display:none;
+            display: none;
         }
     }
-    
+
     td.td-description {
         overflow-y: scroll;
     }
-    th.th-description,td.td-description {
+
+    th.th-description,
+    td.td-description {
         width: 30%;
+
         @media screen and (max-width: 750px) {
             display: none;
         }
     }
-    
-    th.th-title,td.td-title {
+
+    th.th-title,
+    td.td-title {
         /* @media screen and (max-width: 450px){
-            width: 20%;
-        } */
+                width: 20%;
+            } */
     }
-    
-    
-    </style>
+</style>
 @endsection
--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,6 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.app-index')
 
-@section('content')
+@section('content-left')
+<div id="actions">
+    <h6>Account actions</h6>
+    <p><i id="sidebar-icon" class="fa-solid fa-gear"></i><a href="{{route('profile.edit')}}">Settings</a></p>
+    <p><i id="sidebar-icon" class="fa-solid fa-chart-line"></i><a href="{{route('admin.dashboard')}}">Dashboard</a></p>
+</div>
+
+<div id="actions">
+    <h6>Photos actions</h6>
+    <p id="p-edit"><i id="sidebar-icon" class="fa-solid fa-pen-to-square"></i><a
+            href="{{route('admin.categories.edit' ,  $photo)}}">Edit</a></p>
+    <p><i id="sidebar-icon" class="fa-solid fa-compass-drafting"></i><a href="drafts">Drafts</a></p>
+    <p><i id="sidebar-icon" class="fa-solid fa-layer-group"></i><a
+            href="{{route('admin.categories.index')}}">Categories</a></p>
+</div>
+
+<div id="actions">
+    <h6 class="text-danger">Danger Area</h6>
+    <p id="p-delete-show"><i id="sidebar-icon" class="fa-regular fa-trash-can"></i>Delete</p>
+    <div id="div-js-delete-show" class="d-none">
+        <x-modal :photo="$photo"></x-modal>
+    </div>
+</div>
+
+@endsection
+
+@section('content-right')
 
 <x-show-subheader :photo="$photo"></x-show-subheader>
 
@@ -42,7 +68,7 @@
             </span>
             {{$photo->file_size}}Kb
         </p>
-    </div>  
+    </div>
 </div>
 <div class="container">
     <p class="text-center text-dark border border-1">
@@ -53,24 +79,40 @@
         @if($photo->category_id)
         <div style="background-color: #2c2c2c;">
             <p class="text-center text-white" style="background-color: #2c2c2c;"><b
-            class="me-2">Category:</b>{{$photo->category
-            ? $photo->category->name : 'Uncategorized' }}</p>
+                    class="me-2">Category:</b>{{$photo->category
+                ? $photo->category->name : 'Uncategorized' }}</p>
         </div>
         <br>
         @endif
     </div>
 </div>
 
+
 <style>
-        .border-p {
-                    width: 40%;
-                    border-bottom: solid 1.5px black;
-                    border-left: solid 1.5px black;
-                    padding-bottom: .5rem;
-                    padding-left: .2rem;
-                    font-size: medium;
-                    box-shadow: -1px 3px black;
-                }
+    .border-p {
+        width: 40%;
+        border-bottom: solid 1.5px black;
+        border-left: solid 1.5px black;
+        padding-bottom: .5rem;
+        padding-left: .2rem;
+        font-size: medium;
+        box-shadow: -1px 3px black;
+    }
 </style>
 
+<script>
+    let deleteShow = true;
+
+    document.getElementById('p-delete-show').addEventListener('click', function (e) {
+        console.log(e.target);
+        if (deleteShow) {
+            document.getElementById('div-js-delete-show').classList.remove('d-none');
+            deleteShow = !deleteShow;
+        }
+        else {
+            document.getElementById('div-js-delete-show').classList.add('d-none');
+            deleteShow = !deleteShow;
+        }
+    });
+</script>
 @endsection

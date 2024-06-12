56080a594d6a9c8c899012993b3ed0772734fa47
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-12 14:34:43 +0200

    6.4.5th

--- resources/views/components/show-subheader.blade.php
+++ resources/views/components/show-subheader.blade.php
@@ -1,9 +1,9 @@
-<div class="bg-dark bg-gradient py-2">
+<div class="bg-dark bg-gradient py-2 position-relative">
+    <p class="text-white" style="position: absolute; top: 0; left: 1%;"><b>Title:</b></p>
     <div class="container">
-        <h1
-        {{$attributes->merge([
-            'class' => 'text-white'
-        ])}}
-        >{{$photo->title}}</h1>
+        <h1 {{$attributes->merge([
+            'class' => 'text-white pt-1'
+            ])}}
+            >{{$photo->title}}</h1>
     </div>
 </div>

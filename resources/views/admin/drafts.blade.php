9a65deee871f39880270942fde783827f61bbe3e
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-13 12:52:27 +0200

    7.4.5th on draft cards

--- resources/views/admin/drafts.blade.php
+++ resources/views/admin/drafts.blade.php
@@ -6,15 +6,19 @@
 <div class="container">
     <div class="row">
         @forelse ($photos as $key=>$photo )
-        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
-            <div class="card">
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4 my-3 p-2">
+            <div class="card" height="300">
                 @if(Str::startsWith($photo->cover_image , 'https://'))
-                <img class="card-img-top" src="{{$photo->cover_image}}" alt="{{$photo->title}}">
+                <img class="card-img-top position-relative" src="{{$photo->cover_image}}" alt="{{$photo->title}}"
+                    height="200">
                 @else
-                <img class="card-img-top" src="{{asset('storage/' . $photo->cover_image)}}" alt="{{$photo->title}}">
+                <img class="card-img-top position-relative" src="{{asset('storage/' . $photo->cover_image)}}"
+                    alt="{{$photo->title}}" height="200">
                 @endif
-                <div class="card-body">
-                    <p>test</p>
+
+                <div class="card-body" style="height: 100px; overflow-y: scroll;">
+                    <p><b>Title:</b> {{$photo->title}}</p>
+                    <p><b>Description:</b> {{$photo->description}}</p>
                 </div>
             </div>
         </div>

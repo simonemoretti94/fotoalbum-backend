2f921ab3d69af9be1e7b070bcb7d00653d3f47f2
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-14 18:41:59 +0200

    9.3.1th trying to work on scatter plot graph

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Photo;
 
 class DashboardController extends Controller
@@ -10,9 +11,15 @@ class DashboardController extends Controller
     public function index()
     {
         //dd(Photo::all());
+        $photos = Photo::where('user_id', auth()->id())
+            ->where('published', true)
+            ->whereNotNull('category_id')
+            ->get()
+            ->groupBy('category_id');
 
         return view('dashboard', [
-            'photos' => Photo::all(),
+            'photos' => $photos,
+            'categories' => Category::all(),
         ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,14 +23,47 @@
         </div>
     </div>
 
-    <div class="container my-5">
+    <div class="container-fluid my-5">
         <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+    </div>
+
+    <div class="container">
+        @foreach ($photos as $photo)
+        {{$photo}}<br><br>
+        @endforeach
+        
+
+        {{-- @php
+        $xValues = [];
+        $yValues = [];
+        foreach ($photos as $photo):
+           array_push($xValues, $photo->category_id->name)
+           array_push($yValues, $photo->category_id->photos->count())
+       
+        endforeach();
+        @endphp --}}
+        {{-- @php
+        $xValues = [];
+        $yValues = [];
+
+        foreach ($photos as $photo):
+        $category = array_search($photo['category_id'] , $categories); // Ottieni l'oggetto categoria
+
+        array_push($xValues, $category->name);
+        array_push($yValues, $category->photos->count());
+        endforeach;
+        @endphp --}}
+
+        {{-- const xValues = @json($xValues);
+        const yValues = @json($yValues);  these go into script --}}
     </div>
 </div>
 
 <script>
+  
 const xValues = [50,60,70,80,90,100,110,120,130,140,150];
 const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+console.log('values: ', xValues , yValues);
 
 new Chart("myChart", {
   type: "line",
@@ -38,11 +71,11 @@ new Chart("myChart", {
     labels: xValues,
     datasets: [{
       //backgroundColor:"rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
+      //borderColor: "rgba(0,0,255,0.1)",
       data: yValues
     }]
   },
-  //options:{...},
+  //options:{...}
 });
 </script>
 

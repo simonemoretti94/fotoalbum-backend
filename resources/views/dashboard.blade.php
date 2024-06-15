14859b893da8b39c12c47a709be9489730f6b0a7
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-15 09:47:57 +0200

    9.3.5th still on graph infos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,8 +23,13 @@
         </div>
     </div>
 
-    <div class="container-fluid my-5">
-        <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+    <div id="chart-info-wrapper" class="d-flex">
+        <div id="chart-container" class="col-9 container-fluid my-5">
+            <canvas id="myChart"></canvas>
+        </div>
+        <div id="chart-info" class="col-3">
+            <p><b>info: 1</b>Info</p>
+        </div>
     </div>
 
     <div class="container">
@@ -60,20 +65,36 @@
     const yValues = @json($yValues);
     console.log('values: ', xValues, yValues);
     
-    new Chart("myChart", {
-      type: "line",
-      data: {
-        labels: xValues,
-        datasets: [{
-          data: yValues,
-          backgroundColor: 'transparent',
-            borderColor: 'primary',
-            pointBackgroundColor: 'primary',
-            pointBorderColor: 'blue',
-        }]
-      },
-      options: {
-        tooltips: {
+// defining resize x-axis font resize function 
+function fontResize(chart) {
+  let newFontSize;
+  if (window.innerWidth < 600) {
+    newFontSize = 8;
+  } else if (window.innerWidth < 1200) {
+    newFontSize = 12;
+  } else {
+    newFontSize = 16;
+  }
+
+  chart.options.scales.x.ticks.font.size = newFontSize;
+  chart.update();
+}
+
+// creating Chart
+let myChart = new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      data: yValues,
+      backgroundColor: 'transparent',
+      borderColor: 'primary',
+      pointBackgroundColor: 'primary',
+      pointBorderColor: 'blue',
+    }]
+  },
+  options: {
+    tooltips: {
       callbacks: {
         label: function(tooltipItem, data) {
           let label = data.labels[tooltipItem.index];
@@ -82,10 +103,11 @@
               return label + ': ' + value + ' Photos associated';
           }
           else {
-            return label + ': ' + value + ' Photo associated';
+              return label + ': ' + value + ' Photo associated';
           }
         }
-      }
+      },
+      
     },
     scales: {
       y: {
@@ -97,6 +119,11 @@
       x: {
         grid: {
           display: false    // Hide grid background lines
+        },
+        ticks: {
+          font: {
+            size: 15 // Setting default font size
+          }
         }
       }
     },
@@ -104,8 +131,39 @@
       legend: {
         display: true // Hide infos
       }
+    },
+    options: {
+    responsive: true,
+    maintainAspectRatio: false, // needed in order to be responsive
     }
   }
-    });
+});
+
+// changing font-size when refreshing page
+fontResize(myChart);
+
+// updating font-size while resizing the window
+window.addEventListener('resize', function() {
+  fontResize(myChart);
+});
+
     </script>
+    <style>
+        div#chart-info-wrapper {
+            div#chart-container {
+                width: 75%;
+                > div#chartjs-size-monitor {
+                    width: 100%;
+                    > canvas#myChart {
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
+            }
+
+            div#chart-info {
+                width: 25%;
+            }
+        }
+    </style>
 @endsection

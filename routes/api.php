3f9fc0e6cbe44ef0eb91df7c3b0ff15126ca9bdd
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-18 13:12:16 +0200

    11.10th adding leads

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\PhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,6 @@ Route::get('photos', [PhotoController::class, 'index']);
 Route::get('photos/filtered', [PhotoController::class, 'indexFiltered']);
 Route::get('photos/{id}', [PhotoController::class, 'show']);
 
-//Route::post('contacts' , [LeadController::class , 'store']);
+Route::post('contacts', [LeadController::class, 'store']);
 
 Route::get('categories', [PhotoController::class, 'allCategories']);

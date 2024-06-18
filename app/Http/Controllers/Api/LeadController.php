af2125674a10e71ec4c5cbf619a3806944d346a6
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-18 13:27:02 +0200

    12th added mail

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\NewLeadMarkdown;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('guests.leads.create');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request);
+
+        $val_data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required|max:2000',
+        ]);
+
+        $newLead = Lead::create($val_data);
+
+        Mail::to('noreply@fotoalbum.com')->send(new NewLeadMarkdown($newLead));
+
+        return back()->with('Message', 'Message sent successfully');
+    }
 }
---
+++ resources/views/mail/new-lead-message.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New message</title>
+</head>
+
+<body>
+    <h1>You received a new message</h1>
+    <div id="div-img">
+        <p>Name: <span>{{$lead['name']}}</span></p>
+        <p>Name: <span>{{$lead['email']}}</span></p>
+        <p>Name:<span>{{$lead['message']}}</span></p>
+    </div>
+</body>
+
+<style>
+    div#div-img {
+        height: 150px;
+        background-image: url('https://th.bing.com/th/id/OIP.S8fISDPMl1FytnW18RWWCQHaGL?w=960&h=800&rs=1&pid=ImgDetMain');
+        background-image: no-repeat;
+        background-image: cover;
+
+        & p {
+            margin: .5rem auto;
+            color: white;
+
+            >span {
+                color: red;
+                font-weight: 400;
+                font-family: 'Times New Roman', Times, serif;
+                background-color: rgba(255, 255, 255, 0.241);
+            }
+        }
+    }
+</style>
+
+</html>
